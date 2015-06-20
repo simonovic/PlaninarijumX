84ac85d9bba0d3cb5192fed934a6fa1eceb7911c
simonovic <stefan.simon93@gmail.com>
2015-06-20 17:27:26 +0200

    Initial commit

---
+++ app/src/main/java/rs/elfak/mosis/planinarijumx/MainActivity.java
@@ -0,0 +1,57 @@
+package rs.elfak.mosis.planinarijumx;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+public class MainActivity extends Activity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent i = new Intent(this, RegistrationActivity.class);
+        startActivity(i);
+    }
+
+    public void onPrijavaBtn(View view)
+    {
+        Toast.makeText(this, "Radi!", Toast.LENGTH_LONG);
+        EditText ime = (EditText) findViewById(R.id.korIme);
+        EditText loz = (EditText) findViewById(R.id.lozinka);
+        if(ime.getText().toString().trim().equals("") || loz.getText().toString().trim().equals(""))
+            Toast.makeText(this, "Morate uneti i korisničko ime i lozinku!", Toast.LENGTH_LONG).show();
+        else
+        {
+            // provera na sarveru
+            int response;
+            response = 1;
+            //////////////////////
+
+            if(response == 0)
+                Toast.makeText(this, "Pogrešno korisničko ime i/ili lozinka!", Toast.LENGTH_LONG).show();
+            else
+            {
+                Toast.makeText(this, "Pokreni activity!", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    public void onRegBtn(View view)
+    {
+        Intent i = new Intent(this, RegistrationActivity.class);
+        startActivity(i);
+    }
+}
+
+
---
+++ app/src/main/java/rs/elfak/mosis/planinarijumx/RegistrationActivity.java
@@ -0,0 +1,20 @@
+package rs.elfak.mosis.planinarijumx;
+
+import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class RegistrationActivity extends Activity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+
+
+}
