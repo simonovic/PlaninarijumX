a32da9b53c97bee65c2e7f4e20dd3354f1f46d8a
FilipStamenkovic <ficadub@elfak.rs>
2015-06-20 18:01:35 +0200

    Filip

--- app/src/main/java/rs/elfak/mosis/planinarijumx/LogActivity.java
+++ app/src/main/java/rs/elfak/mosis/planinarijumx/LogActivity.java
@@ -2,12 +2,8 @@ package rs.elfak.mosis.planinarijumx;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -18,7 +14,7 @@ public class LogActivity extends Activity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_log);
 
         Intent i = new Intent(this, RegistrationActivity.class);
         startActivity(i);
