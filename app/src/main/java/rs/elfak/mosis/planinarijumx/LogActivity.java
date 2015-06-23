1ecc4b6c3cac79ace84842f2ef483584ba66002b
simonovic <stefan.simon93@gmail.com>
2015-06-23 20:41:48 +0200

    Simon

--- app/src/main/java/rs/elfak/mosis/planinarijumx/LogActivity.java
+++ app/src/main/java/rs/elfak/mosis/planinarijumx/LogActivity.java
@@ -1,7 +1,9 @@
 package rs.elfak.mosis.planinarijumx;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -10,16 +12,35 @@ import android.widget.Toast;
 
 public class LogActivity extends Activity
 {
+    public static final String loginpref = "LoginPref";
+    public static final String userpref = "user";
+    public static final String passpref = "pass";
+    SharedPreferences shPref;
+    EditText user;
+    EditText pass;
+    String username;
+    String password;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log);
+        shPref = getSharedPreferences(loginpref, Context.MODE_PRIVATE);
+        user = (EditText)findViewById(R.id.korIme);
+        pass = (EditText)findViewById(R.id.lozinka);
+        SharedPreferences.Editor editor = shPref.edit();
+        username = shPref.getString(userpref, null);
+        password = shPref.getString(passpref, null);
+        if (username != null)
+        {
+            Intent i = new Intent(this, MainActivity.class);
+            startActivity(i);
+        }
     }
 
     public void onPrijavaBtn(View view)
     {
-        Toast.makeText(getApplicationContext(), "Radi!", Toast.LENGTH_LONG);
         EditText ime = (EditText)findViewById(R.id.korIme);
         EditText loz = (EditText)findViewById(R.id.lozinka);
         if(ime.getText().toString().trim().equals("") || loz.getText().toString().trim().equals(""))
@@ -39,7 +60,15 @@ public class LogActivity extends Activity
                 Toast.makeText(getApplicationContext(), "Pogrešno korisničko ime i/ili lozinka!", Toast.LENGTH_LONG).show();
             else
             {
-                Toast.makeText(getApplicationContext(), "Pokreni activity!", Toast.LENGTH_LONG).show();
+                SharedPreferences.Editor editor = shPref.edit();
+                username = user.getText().toString();
+                password = pass.getText().toString();
+                editor.putString(userpref, username);
+                editor.putString(passpref, password);
+                Intent i = new Intent(this, MainActivity.class);
+                i.putExtra("user", username);
+                i.putExtra("pass", password);
+                startActivity(i);
             }
         }
     }
